99760a7a84ab11c912acf8e553689564266a7d0d
raiqa <rasoolraiqa@gmail.com>
2022-03-13 22:08:29 +0500

    Homepage Done

--- dist/js/main.js
+++ dist/js/main.js
@@ -10,7 +10,7 @@ menubar.onscroll=()=>{
 };
 
 
-// swiper slider initialization
+// hero section swiper slider initialization
 
 var swiper = new Swiper(".home-slider", {
     spaceBetween: 20,
@@ -26,8 +26,37 @@ var swiper = new Swiper(".home-slider", {
 
   // dark to light theme changer
 
-  const theme_changer=document.getElementById("theme-changer");
-
+const theme_changer=document.getElementById("theme-changer");
+const theme_changer_icon=document.getElementById("theme-changer-icon");
   theme_changer.onclick=function(){
     document.body.classList.toggle("light-theme");
+    theme_changer_icon.classList.toggle("fa-sun");
   }
+
+
+  // blog posts slider
+  var swiper = new Swiper(".blogs-slider", {
+    spaceBetween: 20,
+    effect: "slide",
+    grabCursor:true,
+    loop:true,
+    autoplay:{
+      delay:7500,
+      disableOnInteraction:false,
+    },
+    breakpoints:{
+      0:{
+        slidesPerView:1,
+      },
+      768:{
+        slidesPerView:2,
+      },
+      991:{
+        slidesPerView:3,
+      }
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
